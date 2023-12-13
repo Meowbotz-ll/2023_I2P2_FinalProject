0bca7455532c9d80d3bca1692898d7e06adf9ba8
MeowBot <kienhong2000@hotmail.com>
2023-12-14 01:49:16 +0800

    added global

---
+++ global.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+#include <allegro5/allegro_font.h>
+
+#define font_size 12
+#define grid_width 40
+#define grid_height 40
+
+#define window_width 800
+#define window_height 600
+
+extern bool key_state[ALLEGRO_KEY_MAX];
+
+#endif // GLOBAL_H_INCLUDED
